7ee6202ba8ed74dee4abeb6b7abb20d6f7c8fd85
morscor21@gmail.com <morscor21@gmail.com>
2020-12-26 04:31:52 +0100

    beggining to settup classes

---
+++ covid-project/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace covid_project
+{
+    class Citizen
+    {
+        private string _name;
+        private string _cin;
+        private string _colorCode;
+        private Boolean _isAffected;
+        private Boolean _isSuspected;
+        public Citizen(string name ,string cin, string colorCode)
+        {
+            _name = name;
+            _cin = cin;
+            _colorCode = colorCode;
+        }
+
+        //Getters and setters since everything is private in this case
+
+
+
+        private void state()
+        {
+
+        }
+         private Boolean isAlive()
+        {
+            return false;
+        }
+
+        private String generateColorCode()
+        {
+            return "red";
+        }
+    }
+}
---
+++ covid-project/virus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace covid_project
+{
+    class Virus
+    {
+        private Boolean _isDeadly;
+        public Virus()
+        {
+
+        }
+        
+        private bool isDeadly()
+        {
+            return false;
+        }
+
+    }
+}
