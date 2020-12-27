0eaf1ac4dbb0ebc758c5074151a22af7ac35e499
morscor21@gmail.com <morscor21@gmail.com>
2020-12-27 04:22:34 +0100

    refactoring of the Citizen class

--- covid-project/Class1.cs
+++ covid-project/Class1.cs
@@ -6,34 +6,114 @@ namespace covid_project
 {
     class Citizen
     {
+        //Decalring Varibales
         private string _name;
         private string _cin;
         private string _colorCode;
         private Boolean _isAffected;
         private Boolean _isSuspected;
-        public Citizen(string name ,string cin, string colorCode)
+        private Boolean _isAlive;
+        private String _state;
+
+        //contructor
+        public Citizen()
         {
-            _name = name;
-            _cin = cin;
-            _colorCode = colorCode;
         }
 
         //Getters and setters since everything is private in this case
-
-
-
-        private void state()
+        public string name
         {
-
+            get {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
         }
-         private Boolean isAlive()
+        public string cin
         {
-            return false;
+            get
+            {
+                return _cin;
+            }
+            set
+            {
+                _cin = value;
+            }
+        }
+        public string colorCode
+        {
+            get
+            {
+                return _colorCode;
+            }
+            set
+            {
+                _colorCode = value;
+            }
+        }
+        public Boolean isAffected
+        {
+            get
+            {
+                return _isAffected;
+            }
+            set
+            {
+                _isAffected = value;
+            }
+        }
+        public Boolean isSuspected
+        {
+            get
+            {
+                return _isSuspected;
+            }
+            set
+            {
+                _isSuspected = value;
+            }
+        }
+        public Boolean isAlive
+        {
+            get
+            {
+                return _isAlive;
+            }
+            set
+            {
+                _isAlive = value;
+            }
+        }
+        public String state
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                _state = value;
+            }
         }
 
         private String generateColorCode()
         {
-            return "red";
+            if (this.isSuspected)
+            {
+                //this will be the in-betwwen case 
+                colorCode = "orange";
+            }else if (this.isAffected)
+            {
+                //the affected citizens will be marked with red
+                colorCode = "red";
+            }else if (!this.isAffected)
+            {
+                //the non-effected will be marked with green
+                colorCode = "green";
+            }
+            return colorCode;
         }
     }
 }
