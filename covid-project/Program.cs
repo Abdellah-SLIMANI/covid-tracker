5708edbeaf8f76dfd76846c20640a231f351f18f
morscor21@gmail.com <morscor21@gmail.com>
2021-01-05 06:21:20 +0100

    adding files

--- covid-project/Program.cs
+++ covid-project/Program.cs
@@ -6,11 +6,6 @@ namespace covid_project
     {
         static void Main(string[] args)
         {
-            Citizen cit1 = new Citizen("mhmd","UC15554");
-            cit1.isAffected = true;
-            Cure cure1 = new Cure();
-            cure1.name = "covidKiller";
-            cure1.isSafe = false;
         }
     }
 }
