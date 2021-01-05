204aea93639ff1d037d5d8f7039a571b4899960e
morscor21@gmail.com <morscor21@gmail.com>
2021-01-05 06:47:25 +0100

    seeting the front end up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="header">
+        <img/>
+      </div>
+      <div className="tableContainer">
+        <table>
+            <td>Hello</td>
+        </table>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default connect()(App);
